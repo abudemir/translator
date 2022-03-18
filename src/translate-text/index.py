779648b9f8ca479e4a15c87d0b5125d3257ac840
abudemir <98801891+abudemir@users.noreply.github.com>
2022-03-18 21:33:03 +0400

    Update index.py

--- src/translate-text/index.py
+++ src/translate-text/index.py
@@ -1,23 +1,31 @@
 import json
 import logging
+import boto3
 
 from src.output_formatter import get_success_output, get_error_output
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+def handler(event, _):
+    logger.info("Echo invocable")
+    args = json.loads(event)["body"]
 
-"""
-Write your code here.
-"""
-def handler(event, context):
-    logger.info("Context enabled Echo invocable")
     try:
-        body = json.loads(event)['body']
-        context = json.loads(event).get('context', {})
-        flow_execution_id = context.get('flow_execution_id', None)
-        if not flow_execution_id:
-            return get_error_output(400, "Flow Execution ID (flow_execution_id) not specified in context")
-        return get_success_output(200, body)
+        tl = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
+        logger.info("Translation client creation is a success!")
+        text=args["text"]
+        src_language = args["SourceLanguageCode"]
+        target_language = args["TargetLanguageCode"]
+
+        tl_text = tl.translate_text(Text=text, 
+            SourceLanguageCode=src_language, TargetLanguageCode=target_language)
+        logger.info("Translation complete for one single text")
+
+        response = {"TranslatedText":tl_text,"SourceLanguageCode":src_language,"TargetLanguageCode":target_language}
+        
+        return get_success_output(status_code=200, body=json.dumps(response))
+    except KeyError:    
+        return get_error_output(400,"Missing Arguments","Invalid Request Error")
     except:
         return get_error_output(500, "Dummy error message", "Internal Server Error")
